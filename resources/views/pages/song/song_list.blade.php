e57d6b476a511afb3327ab27f1fa5f7f215a8bba
phanquynh <ptnquynh.20it5@vku.udn.vn>
2022-06-14 21:43:05 +0700

    add data

--- resources/views/pages/song/song_list.blade.php
+++ resources/views/pages/song/song_list.blade.php
@@ -9,7 +9,7 @@
           <div class="card mb-4">
             <div class="card-header pb-0">
             <h6>Song List</h6>
-              <a href="{{ route('song.store') }}"><span class="badge badge-sm bg-gradient-success" style="margin-right: 10px; float: right; word-wrap: normal;">Add Song</span></a>
+              <a href="{{ route('song.create') }}"><span class="badge badge-sm bg-gradient-success" style="margin-right: 10px; float: right; word-wrap: normal;">Add Song</span></a>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
